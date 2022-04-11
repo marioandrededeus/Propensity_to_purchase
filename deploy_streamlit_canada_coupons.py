a95c2852db94918fd81a51ab2db227b0652fcb9c
Mario Andre de deus <47201477+marioandrededeus@users.noreply.github.com>
2022-04-11 01:30:14 -0300

    Update deploy_streamlit_canada_coupons.py

--- deploy_streamlit_canada_coupons.py
+++ deploy_streamlit_canada_coupons.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import sklearn
+#import sklearn
 from sklearn.ensemble import RandomForestClassifier
-import base64
+#import base64
 import pickle
 
 #streamlit
